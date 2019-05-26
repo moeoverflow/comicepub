56da155ed9942343bf8737d8cbd4a45238cb9ef5
ShinCurry <shincurryyang@gmail.com>
2019-05-26 23:25:22 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="comicepub",
+    version="0.1.0",
+    author="ShinCurry",
+    author_email="shincurryyang@gmail.com",
+    description="Japanese comic EPUB3 generate tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moeoverflow/comicepub",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'Jinja2>=2.10.1'
+    ],
+)
